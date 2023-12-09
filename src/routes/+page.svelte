90506e7e6613fe1ef73b174dcc8211429a4a9b37
ratinac-nikola <ratinac.nikola@gmail.com>
2023-12-09 03:20:42 +0100

    solved parallel requests

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
 <div
 	class="snap-y container grid grid-rows-2 m-auto gap-5 overflow-y-scroll mt-3"
 >
-	<CardList listIdentifier="alltimebest" />
+	<CardList listIdentifier="1" />
+	<CardList listIdentifier="2" />
 	<!-- <CardList listIdentifier="alltimebest" />
 	<CardList listIdentifier="alltimebest" />
 	<CardList listIdentifier="alltimebest" /> -->
