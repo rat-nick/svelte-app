4ba5055ad228e4e8318bcc47551cedad3a38aaa0
ratinac-nikola <ratinac.nikola@gmail.com>
2023-12-13 19:31:39 +0100

    add IconButton component

---
+++ src/components/IconButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let variant: string = 'success';
+	export let size: 'sm' | 'md' | 'lg' | 'xl' = 'sm';
+	const variantMapping: { [id: string]: string } = {
+		success: 'bg-gradient-to-br variant-gradient-primary-tertiary',
+		info: 'bg-gradient-to-br variant-gradient-secondary-tertiary',
+		error: 'bg-gradient-to-br bg-gradient-to-br variant-gradient-warning-error'
+	};
+</script>
+
+<button
+	on:click
+	class={`btn btn-icon-${size} text-xl ${variantMapping[variant]}`}
+>
+	<slot />
+</button>
